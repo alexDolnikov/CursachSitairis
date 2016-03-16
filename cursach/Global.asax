6dbeb36680e0f2ab527e20c362c09e6e31c1ad40
Александр Александр < >
2016-03-16 20:54:46 +0200

    Inital commit

---
+++ cursach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cursach.MvcApplication" Language="C#" %>
---
+++ cursach/Migrations/201603161818447_Add_Email_to_Test.cs
@@ -0,0 +1,27 @@
+namespace cursach.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_Email_to_Test : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.UserProfile",
+                c => new
+                    {
+                        UserId = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.UserProfile");
+        }
+    }
+}
